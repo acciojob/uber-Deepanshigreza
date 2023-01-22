fb21702f618c83ee1d8a7038092b37b4d78b81c8
Deepanshigreza <deepanshigreza@gmail.com>
2023-01-22 15:10:42 +0530

    done

--- src/main/java/com/driver/model/Customer.java
+++ src/main/java/com/driver/model/Customer.java
@@ -15,4 +15,36 @@ public class Customer {
 
     @OneToMany(mappedBy = "customer",cascade =CascadeType.ALL)
     List<TripBooking> tripBookingList=new ArrayList<>();
+
+    public Integer getCustomerId() {
+        return CustomerId;
+    }
+
+    public void setCustomerId(Integer customerId) {
+        CustomerId = customerId;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<TripBooking> getTripBookingList() {
+        return tripBookingList;
+    }
+
+    public void setTripBookingList(List<TripBooking> tripBookingList) {
+        this.tripBookingList = tripBookingList;
+    }
 }
